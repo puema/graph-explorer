dcf0b93e19e8221b0bb9fb8b03974022976eabfc
Marcel Puetz <marcel.puetz@netlight.com>
2021-04-23 10:10:09 +0200

    👆 Add dragging nodes

--- src/graph.ts
+++ src/graph.ts
@@ -3,8 +3,10 @@ import {
   forceLink,
   forceManyBody,
   forceSimulation,
+  Simulation,
   SimulationNodeDatum,
 } from 'd3-force';
+import { drag } from 'd3-drag';
 import { select } from 'd3-selection';
 
 import data from './data.json';
@@ -24,12 +26,37 @@ export function createGraph() {
       forceCenter(document.body.offsetWidth / 2, document.body.offsetHeight / 2)
     );
 
+  const addDrag = (simulation: Simulation<Node, undefined>) => {
+    function handleStart(event: any) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    }
+
+    function handleDrag(event: any) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
+    }
+
+    function handleEnd(event: any) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+
+    return drag()
+      .on('start', handleStart)
+      .on('drag', handleDrag)
+      .on('end', handleEnd);
+  };
+
   const nodeElements = select('body')
     .selectAll('graph-node')
     .data(nodes)
     .enter()
     .append('graph-node')
-    .attr('data-id', (d) => d.id);
+    .attr('data-id', (d) => d.id)
+    .call(addDrag(simulation) as any);
 
   simulation.on('tick', () => {
     nodeElements
